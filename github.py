e19f4b2671ad97df1fc127be808f243b22644749
Doug Mahugh <dmahugh@microsoft.com>
2016-03-05 00:19:20 -0800

    add org_repos() function

--- github.py
+++ github.py
@@ -1,8 +1,13 @@
 """GitHub helper functions.
 
-link_url -> Extract link URL from HTTP header returned by GitHub API.
+basic_auth() -> Credentials for basic authentication.
+link_url() ---> Extract link URL from HTTP header returned by GitHub API.
+org_repos() --> Get all repos for an organization.
 """
+import collections
+import json
 import os
+import requests
 
 #------------------------------------------------------------------------------
 def basic_auth():
@@ -33,6 +38,28 @@ def link_url(link_header, linktype='next'):
             retval = link.split(';')[0].strip()[1:-1]
     return retval
 
+#-------------------------------------------------------------------------------
+def org_repos(org='microsoft'):
+    """Get all repos for an organization.
+
+    org = organization Name
+    Returns a list of namedtuple objects, one per repo.
+    """
+    FIELDS = ['full_name', 'watchers', 'forks', 'open_issues', 'default_branch']
+    URL = 'https://api.github.com/orgs/' + org + '/repos'
+    repolist = [] # the list that will be returned
+    Repo = collections.namedtuple('Repo', ' '.join(FIELDS))
+
+    response = requests.get(URL, auth=basic_auth())
+    if response.ok:
+        thispage = json.loads(response.text)
+        for repo_json in thispage:
+            repo_nt = Repo(repo_json['full_name'], repo_json['watchers'],
+                           repo_json['forks'], repo_json['open_issues'],
+                           repo_json['default_branch'])
+            repolist.append(repo_nt)
+    return repolist
+
 # if running standalone, run a few simple tests --------------------------------
 if __name__ == "__main__":
 
@@ -44,3 +71,9 @@ if __name__ == "__main__":
     for reltype in ['first', 'prev', 'next', 'last']:
         URL = link_url(TESTLINKS, reltype)
         print(reltype, URL)
+
+    print('-'*40 + '\n' + 'org_repos() test' + '\n' + '-'*40)
+    ms_repos = org_repos('microsoft')
+    for repo in ms_repos:
+        print(repo)
+    print('Total repos: ', len(ms_repos))
