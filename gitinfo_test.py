79e15f1aff5a7de762ba28a1a20a9b4b73ebb345
Doug Mahugh <dmahugh@microsoft.com>
2016-04-07 20:22:40 -0700

    add auth_user() tests

--- gitinfo_test.py
+++ gitinfo_test.py
@@ -1,21 +1,25 @@
 """Test suite for gitinfo module
 """
 import pytest
-# pragma pylint: disable=R0201,C0111,W0621
+# pragma pylint: disable=R0201,C0103,C0111,W0621
 
 import gitinfo as gi
 
 #-------------------------------------------------------------------------------
-@pytest.fixture
-def linkheader():
-    """Test data for testing gitinfo.pagination() function.
+class Test_auth_user():
+    """Tests for gitinfo.auth_user() function.
     """
-    return "<https://api.github.com/organizations/6154722/" + \
-        "repos?page=2>; rel=\"next\", <https://api.github.com/" + \
-        "organizations/6154722/repos?page=18>; rel=\"last\""
+    def test_noconfig(self):
+        assert gi.auth_user() is None
+
+    def test_msftgits(self):
+        gi.auth_config({'username': 'msftgits'})
+        authtuple = gi.auth_user()
+        assert authtuple[0] == 'msftgits'
+        assert len(authtuple[1]) == 40
 
 #-------------------------------------------------------------------------------
-class TestPagination():
+class Test_pagination():
     """Tests for gitinfo.pagination() function.
     """
     def test_first(self, linkheader):
@@ -37,3 +41,12 @@ class TestPagination():
         linkdict = gi.pagination(linkheader)
         assert linkdict['prevpage'] == 0
         assert linkdict['prevURL'] is None
+
+@pytest.fixture
+def linkheader():
+    """Test data for testing gitinfo.pagination() function.
+    """
+    return "<https://api.github.com/organizations/6154722/" + \
+        "repos?page=2>; rel=\"next\", <https://api.github.com/" + \
+        "organizations/6154722/repos?page=18>; rel=\"last\""
+
