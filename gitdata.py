698d597ed0967fa0cf697a63b8754c6a5a925105
Doug Mahugh <dmahugh@microsoft.com>
2016-06-24 20:35:28 -0700

    add constants to '*' and 'nourls' options

--- gitdata.py
+++ gitdata.py
@@ -305,6 +305,8 @@ def data_fields(*, entity=None, jsondata=None, fields=None, constants=None):
 
     if fields[0] in ['*', 'urls', 'nourls']:
         # special cases to return all fields or all url/non-url fields
+        if fields[0] in ['*', 'nourls']:
+            values.update(constants)
         for fldname in jsondata:
             if fields[0] == '*' or \
                 (fields[0] == 'urls' and fldname.endswith('url')) or \
