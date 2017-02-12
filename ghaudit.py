9ea902791bad91440dcd6e3c420aa135d31e4bc7
Doug Mahugh <dmahugh@microsoft.com>
2017-02-11 18:48:56 -0800

    stub out teams/repos data generation

--- ghaudit.py
+++ ghaudit.py
@@ -18,8 +18,10 @@ def getmsdata():
     """Retrieve/refresh all Microsoft data needed for audit reports.
     """
 
-   # orgs.csv
-   # command line: gitdata orgs -amsftgits -sa -nghaudit/orgs.csv -flogin/user/id
+   # create the ORG data file, list of organizations to be audited
+   # Below is inline automation of this command:
+   #   gitdata orgs -amsftgits -sa -nghaudit/orgs.csv -flogin/user/id
+    orgfile = 'ghaudit/orgs.csv'
     authuser = 'msftgits'
     gd._settings.display_data = True
     gd._settings.verbose = False
@@ -31,13 +33,22 @@ def getmsdata():
         constants={"user": authuser}, headers={})
     sorted_data = sorted(templist, key=gd.data_sort)
     gd.data_display(sorted_data)
-    gd.data_write('ghaudit/orgs.csv', sorted_data)
+    gd.data_write(orgfile, sorted_data)
 
-    #/// step through orgs.csv to handle these:
-    #/// ghaudit/teams.csv = list of all teams for each org
-    #/// ghaudit/repos.csv = list of all repos for each org
+    # create the TEAM and REPO data files, iterating over ORGs
+    teamfile = 'ghaudit/teams.csv'
+    repofile = 'ghaudit/repos.csv'
+    open(teamfile, 'w').write('///header') # initialize data file
+    open(repofile, 'w').write('///header') # initialize data file
+    for line in open(orgfile, 'r').readlines():
+        orgname = line.split(',')[0]
+        #/// get team data for this org
+        open(teamfile, 'a').write('///org=' + orgname)
+        #/// get repo data for this org
+        open(repofile, 'a').write('///org=' + orgname)
 
-    #/// step through repos.csv to create the collabs list:
+    # create the COLLAB data file, iterating over REPOs
+    #/// step through repofile to create the collabs list:
     #/// ghaudit/collabs.csv = all collaborators for each repo
 
 #-------------------------------------------------------------------------------
